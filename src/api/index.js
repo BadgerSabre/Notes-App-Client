a379b9da977c65788ddcfdb0b238cb2cf625042b
Brandon Dega <brandondega@gmail.com>
2022-10-06 18:39:26 -0500

    tweaked backend URLs

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'https://notes-app-bdega-server.herokuapp.com//notes';
-const authURL = 'https://notes-app-bdega-server.herokuapp.com//auth'
+const url = 'https://notes-app-bdega-server.herokuapp.com/notes';
+const authURL = 'https://notes-app-bdega-server.herokuapp.com/auth'
 
 export const fetchUserNotes = (uid) => axios.get(`${url}/${uid}`)
 export const login = (loginCredientials) => axios.post(`${authURL}/login`,loginCredientials)
